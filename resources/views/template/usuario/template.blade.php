062b38f3e4a4fbca309ae16a644a94294dd9f84b
dasebra <90878769+dasebra@users.noreply.github.com>
2026-04-10 14:11:04 -0400

    .

--- resources/views/template/usuario/template.blade.php
+++ resources/views/template/usuario/template.blade.php
@@ -52,6 +52,10 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
     @yield('page-styles')
 
 </head>
