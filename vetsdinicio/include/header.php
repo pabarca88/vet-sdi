b594adc47019c6f04f24645e236968e88707e242
dasebra <90878769+dasebra@users.noreply.github.com>
2026-04-24 19:03:46 -0400

    VET SDI INICIO
    Pagina informativa del sistema VET

---
+++ vetsdinicio/include/header.php
@@ -0,0 +1,31 @@
+<header>
+		<!--Menú 1-->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			<a class="navbar-brand" href="index.php">
+			    <img src="assets/images/logo.png" width="180" height="auto" alt="VETSDI">
+			  </a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+				<ul class="navbar-nav ml-auto mt-2 mt-lg-0 align-items-center">
+					<li class="nav-item mx-2 active">
+						<a class="nav-link ancla" href="#funcionalidad" data-ancla="funcionalidad">Funcionalidades</a>
+					</li>
+					<li class="nav-item mx-2">
+						<a class="nav-link ancla" href="#contacto" data-ancla="contacto">Contacto</a>
+					</li>
+					<!--<li class="nav-item mx-2">
+						<a class="nav-link ancla" href="#contacto" data-ancla="contacto">Contacto</a>
+					</li>-->
+					<li class="nav-item mx-2">
+						<a class="btn btn-info rounden-xl" href="https://www.vet-sdi.cl/Ingreso"> <i class="fa-solid fa-arrow-right-to-bracket"></i> INGRESAR</a>
+					</li>
+					<li class="nav-item mx-2">
+						<a class="btn  btn-purple rounden-xl " href="#" role="button" data-toggle="modal"><i class="fa-solid fa-calendar-days"></i> AGENDAR HORA</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+</header>
